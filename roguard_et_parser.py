11fa3c7046046edd5f2d6f0443228e93413cceab
Joseph Rafael Ferrer <rafael2x0@gmail.com>
2019-01-18 19:41:24 +0800

    Changed caching to use Python's pickle to save space.

--- roguard_et_parser.py
+++ roguard_et_parser.py
@@ -1,11 +1,12 @@
 import re
+import pickle
 import os.path
 import time
 import urllib.request
 from bs4 import BeautifulSoup
 
 # Time in minutes to update
-UPD_MINUTES = 60
+UPD_MINUTES = 600
 
 # ROGuard Endless Tower URL
 URL = 'http://www.roguard.net/game/endless-tower/'
@@ -13,25 +14,9 @@ URL = 'http://www.roguard.net/game/endless-tower/'
 #-------------------------------------------------------------------
 
 UPD_SEC = UPD_MINUTES * 60
-CACHE_FNAME = 'ET_dump.txt'
+CACHE_FNAME = 'ET_dump.pickle'
 IDMAP_FNAME = 'roguard_mon_id.txt'
 
-def is_valid_channel(channel):
-    # check for valid channels ourselves because ROGuard doesn't
-    #TODO
-    return True
-
-def check_cache():
-    # get new data if cached one is older than desired
-    # update time; create a cache if necessary
-    if not os.path.isfile(CACHE_FNAME) or \
-        time.time() - os.path.getmtime(CACHE_FNAME) > UPD_SEC:
-
-        with urllib.request.urlopen(URL) as response:
-            webpage = response.read()
-            with open(CACHE_FNAME, 'w') as f:
-                print(webpage, file=f)
-                print('---UPDATED ENDLESS TOWER DATA---\n')
 
 def parse_monster_id_map():
     id_map = {}
@@ -41,52 +26,11 @@ def parse_monster_id_map():
             id_map[int(num)] = monster
     return id_map
 
-def get_floor(monster, soup):
-    matches = []
-
-    for target in soup.find_all('a', title=re.compile(monster)):
-        p = target.find_parent('td')
-        i = 1
-        channel = p.find_previous_sibling('td').string
-        while channel is None:
-            p = p.find_previous_sibling('td')
-            channel = p.find_previous_sibling('td').string
-            i += 1
-
-            #TOREMOVE
-            if i > 100:
-               assert False, 'Bug in the program'
-
-        floor = i*10
-        if is_valid_channel(channel):
-            matches.append((floor, channel))
-
-    matches.sort(key=lambda x: x[0])
-
-    for floor, channel in matches:
-        print(channel, 'Floor', floor)
-
-def test(soup):
-    monster_tables = soup.find_all('table')
-
-    mvp_table = monster_tables[0].find('tbody')
-    mini_table = monster_tables[0].find('tbody')
-
-    parse_table(mvp_table)
-
-def monster_id_mapper(soup):
+def monster_id_utility(soup):
+    # Prints out monster ID to monster name mapping
     id_map = {}
 
-    # pattern = re.compile(r'"(\d*)"\s*>\s*(\S*)\s*<')
-    # for v in soup.find_all('option'):
-    #     match = pattern.search(str(v))
-    #     if match:
-    #         id_map[int(match.group(1))] = match.group(2)
-
-    # for k, v in id_map.items():
-    #     print("%s: '%s'" % (k, v))
-
-    pattern = re.compile(r'\/db\/monsters\/(\d*)\/.*title="(\S*)')
+    pattern = re.compile(r'\/db\/monsters\/(\d*)\/.*title.*"(.*) -')
     for v in soup.find_all('a'):
         match = pattern.search(str(v))
         if match:
@@ -96,19 +40,61 @@ def monster_id_mapper(soup):
         d = id_map[k]
         print("%s: %s" % (k, d))
 
+def parse_data(soup):
+
+    pattern = re.compile(r'(\d{1,3})F')
+    et_data = {}
+
+    def get_floors(table):
+        floors = []
+        for data in table.find_all('th')[1:]: # first entry is empty
+            match = pattern.search(str(data))
+            floors.append(int(match.group(1)))
+        return floors
+
+
+    def build_data(mob_list, floors):
+        for channel_id, channel_mobs in mob_list.items():
+            for floor, floor_mobs in zip(floors, channel_mobs):
+                if floor not in et_data:
+                    et_data[floor] = {}
+                et_data[floor][channel_id] = floor_mobs
+        return et_data
+
+    monster_tables = soup.find_all('table')
+
+    mvp_floors = get_floors(monster_tables[0])
+    mvp_list = parse_table(monster_tables[0].find('tbody'))
+    build_data(mvp_list, mvp_floors)
+
+    mini_list = parse_table(monster_tables[1].find('tbody'))
+    mini_floors = get_floors(monster_tables[1])
+    build_data(mini_list, mini_floors)
+
+    return et_data
+
 def parse_table(table):
 
     channel_id_pattern = re.compile(r'.*>\s*(\d)\s*<.*')
-    monster_id_pattern = re.compile(r'href\s*=\s*"\/db\/monsters\/(\d.*)\/"')
+    monster_id_pattern = re.compile(r'\/db\/monsters\/(\d*)\/')
+
+    id_map = parse_monster_id_map()
 
     def get_channel_id(columns):
         # get channel ID on the leftmost column
         return int(channel_id_pattern.match(str(columns[0])).group(1))
 
     def parse_columns(columns):
-        pass
+        floors = []
+        for column in columns[1:]: # skip first column
+            mons_in_floor = []
+            for match in monster_id_pattern.finditer(str(column)):
+                mons_in_floor.append(id_map[int(match.group(1))])
+            floors.append(mons_in_floor)
+        return floors
+            
 
-    contents = []
+    contents = {i: None for i in range(10)}
     for i, channel in enumerate(table.children):
         if i % 2 == 1: # useful data are on odd indices
 
@@ -116,45 +102,33 @@ def parse_table(table):
             
             channel_id = get_channel_id(columns)
 
-            print(channel_id)
-            assert False
-
-            contents.append(channel)
+            floors = parse_columns(columns)
+            contents[channel_id] = floors
+            
     return contents
+
+def check_cache():
+    # get new data if cached one is older than desired
+    # update time; create a cache if necessary
+    if not os.path.isfile(CACHE_FNAME) or \
+        time.time() - os.path.getmtime(CACHE_FNAME) > UPD_SEC:
+
+        with urllib.request.urlopen(URL) as response:
+            webpage = response.read()
+            soup = BeautifulSoup(webpage, 'html.parser')
+            et_data = parse_data(soup)
+
+            with open(CACHE_FNAME, 'wb') as f:
+                pickle.dump(et_data, f)
+                print('---UPDATED ENDLESS TOWER DATA---\n')
 
 
 def read_et_list(targets):
-
     check_cache()
 
-    with open(CACHE_FNAME) as cached_response:
-        webpage = cached_response.read()
-        soup = BeautifulSoup(webpage, 'html.parser')
+    with open(CACHE_FNAME, 'rb') as f:
+        et_data = pickle.load(f)
 
-        # TESTING
-        #test(soup)
-        
-        matches = []
-
-        for target in soup.find_all('a', title=re.compile('Mistress')):
-            p = target.find_parent('td')
-            i = 1
-            channel = p.find_previous_sibling('td').string
-            while channel is None:
-                p = p.find_previous_sibling('td')
-                channel = p.find_previous_sibling('td').string
-                i += 1
-                if i > 100:
-                   assert False, 'Bug in the program'
-
-            floor = i*10
-            if is_valid_channel(channel):
-                matches.append((floor, channel))
-
-        matches.sort(key=lambda x: x[0])
-
-        for floor, channel in matches:
-            print(channel, 'Floor', floor)
 
 read_et_list([])
     
